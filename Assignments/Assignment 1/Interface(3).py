b72b0a9aebe687339db6f30d41fa831b086c0449
Aditya <aditya-may9@users.noreply.github.com>
2018-01-26 10:17:40 -0700

    Adding files to git

---
+++ Assignments/Assignment 1/Interface(3).py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2.7
+#
+# Interface for the assignement
+#
+
+import psycopg2
+
+DATABASE_NAME = 'dds_assgn1'
+
+
+def getopenconnection(user='postgres', password='1234', dbname='dds_assgn1'):
+    return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
+
+
+def loadratings(ratingstablename, ratingsfilepath, openconnection):
+    pass
+
+
+def rangepartition(ratingstablename, numberofpartitions, openconnection):
+    pass
+
+
+def roundrobinpartition(ratingstablename, numberofpartitions, openconnection):
+    pass
+
+
+def roundrobininsert(ratingstablename, userid, itemid, rating, openconnection):
+    pass
+
+
+def rangeinsert(ratingstablename, userid, itemid, rating, openconnection):
+    pass
+
+def deletepartitionsandexit(openconnection):
+    pass
+
+def create_db(dbname):
+    """
+    We create a DB by connecting to the default user and database of Postgres
+    The function first checks if an existing database exists for a given name, else creates it.
+    :return:None
+    """
+    # Connect to the default database
+    con = getopenconnection(dbname='postgres')
+    con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = con.cursor()
+
+    # Check if an existing database with the same name exists
+    cur.execute('SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname=\'%s\'' % (dbname,))
+    count = cur.fetchone()[0]
+    if count == 0:
+        cur.execute('CREATE DATABASE %s' % (dbname,))  # Create the database
+    else:
+        print 'A database named {0} already exists'.format(dbname)
+
+    # Clean up
+    cur.close()
+    con.close()

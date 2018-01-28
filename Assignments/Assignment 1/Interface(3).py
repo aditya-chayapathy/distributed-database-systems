daa9f49efc136fa0c03c92f452fb755521ca8671
Aditya <aditya.chayapathy@gmail.com>
2018-01-27 20:24:51 -0700

    task 1 complete

--- Assignments/Assignment 1/Interface(3).py
+++ Assignments/Assignment 1/Interface(3).py
@@ -12,8 +12,60 @@ def getopenconnection(user='postgres', password='1234', dbname='dds_assgn1'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
 
 
+def check_if_table_exists(table_name, openconnection):
+    cur = openconnection.cursor()
+
+    cur.execute('select current_database()')
+    db_name = cur.fetchall()[0][0]
+
+    cur.execute('select count(*) from information_schema.tables where table_name=\'' + table_name + '\' and table_catalog=\'' + db_name + '\'')
+    count = cur.fetchall()[0][0]
+
+    cur.close()
+
+    if count == 1:
+        return True
+    else:
+        return False
+
+
+def create_table(table_name, openconnection):
+    create_command = ""
+    if table_name == 'metadata':
+        create_command = 'create table metadata (current_round_robin_fragment int, current_range_fragment int)'
+    elif table_name == 'ratings':
+        create_command = 'create table ratings (userid int, movieid int, rating real)'
+    elif 'ratings' in table_name:
+        create_command = 'create table ' + table_name + ' (userid int, movieid int, rating real)'
+    else:
+        print "UNKNOWN TABLE. ABORTING !!!"
+        exit(2)
+
+    cur = openconnection.cursor()
+    cur.execute(create_command)
+    openconnection.commit()
+    cur.close()
+
+
+def insert_ratings_record_to_table(userid, movieid, ratings, table_name, openconnection):
+    cur = openconnection.cursor()
+    cur.execute("insert into " + table_name + " values (" + userid + "," + movieid + "," + ratings + ")")
+    openconnection.commit()
+    cur.close()
+
+
 def loadratings(ratingstablename, ratingsfilepath, openconnection):
-    pass
+    if not check_if_table_exists(ratingstablename, openconnection):
+        create_table(ratingstablename, openconnection)
+
+    if not check_if_table_exists('metadata', openconnection):
+        create_table('metadata', openconnection)
+
+    with open(ratingsfilepath, 'r') as input_file:
+        file_data = input_file.read()
+        for line in file_data.split("\n"):
+            userid, movieid, rating, timestamp = line.split("::")
+            insert_ratings_record_to_table(userid, movieid, rating, "ratings", openconnection)
 
 
 def rangepartition(ratingstablename, numberofpartitions, openconnection):
@@ -31,8 +83,10 @@ def roundrobininsert(ratingstablename, userid, itemid, rating, openconnection):
 def rangeinsert(ratingstablename, userid, itemid, rating, openconnection):
     pass
 
+
 def deletepartitionsandexit(openconnection):
     pass
+
 
 def create_db(dbname):
     """

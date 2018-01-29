e58b7bc751eea0ade02fa1679790bdeac0ffba7b
Aditya <aditya-may9@users.noreply.github.com>
2018-01-29 10:17:01 -0700

    round robin insert fix

--- Assignments/Assignment 1/Interface.py
+++ Assignments/Assignment 1/Interface.py
@@ -217,17 +217,3 @@ def create_db(dbname):
     # Clean up
     cur.close()
     con.close()
-
-
-if __name__ == "__main__":
-    con = getopenconnection(dbname='postgres')
-    loadratings("ratings", "test_data.dat", con)
-    roundrobinpartition("ratings", 5, con)
-    roundrobininsert("ratings", 1, 2, 3, con)
-    roundrobininsert("ratings", 2, 2, 13, con)
-    roundrobininsert("ratings", 3, 9, 3, con)
-    roundrobininsert("ratings", 4, 2, 13, con)
-    roundrobininsert("ratings", 5, 7, 33, con)
-    roundrobininsert("ratings", 6, 2, 3, con)
-    roundrobininsert("ratings", 6, 2, 3, con)
-    deletepartitionsandexit(con)

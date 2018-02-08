257970e4f80187a674e9e40cbcb623c9dbb61ac2
Aditya <aditya-may9@users.noreply.github.com>
2018-02-07 22:23:08 -0700

    Adding assignment 2 files

---
+++ Assignments/Assignment 2/Assignment2_Interface.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.7
+#
+# Assignment2 Interface
+#
+
+import psycopg2
+import os
+import sys
+# Donot close the connection inside this file i.e. do not perform openconnection.close()
+def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection):
+    #Implement RangeQuery Here.
+    pass #Remove this once you are done with implementation
+
+def PointQuery(ratingsTableName, ratingValue, openconnection):
+    #Implement PointQuery Here.
+    pass # Remove this once you are done with implementation

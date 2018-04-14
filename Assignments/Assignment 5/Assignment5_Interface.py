97f8dd93e2cfc9a1a6eeb00f8ed04984b78a9b1c
Aditya <aditya-may9@users.noreply.github.com>
2018-04-14 10:31:33 -0700

    Adding assignment 5

---
+++ Assignments/Assignment 5/Assignment5_Interface.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+#
+# Assignment5 Interface
+# Name: 
+#
+
+from pymongo import MongoClient
+import os
+import sys
+import json
+
+def FindBusinessBasedOnCity(cityToSearch, saveLocation1, collection):
+    pass
+
+def FindBusinessBasedOnLocation(categoriesToSearch, myLocation, maxDistance, saveLocation2, collection):
+    pass
+

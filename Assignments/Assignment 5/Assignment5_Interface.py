3524e6c33156525b7a81c89e3229ec2a76b9bc0d
Aditya <aditya-may9@users.noreply.github.com>
2018-04-14 19:50:04 -0700

    Adding the two functions

--- Assignments/Assignment 5/Assignment5_Interface.py
+++ Assignments/Assignment 5/Assignment5_Interface.py
@@ -8,10 +8,42 @@ from pymongo import MongoClient
 import os
 import sys
 import json
+import math
+
+
+def get_distance_between_points(point2_lat, point2_long, point1_lat, point1_long):
+    r = 3959
+    phi1 = math.radians(point1_lat)
+    phi2 = math.radians(point2_lat)
+    delta_phi = math.radians(point2_lat - point1_lat)
+    delta_lambda = math.radians(point2_long - point1_long)
+    a = math.sin(delta_phi / 2) * math.sin(delta_phi / 2) + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) * math.sin(delta_lambda / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = r * c
+
+    return distance
+
 
 def FindBusinessBasedOnCity(cityToSearch, saveLocation1, collection):
-    pass
+    result = collection.find({"city": cityToSearch})
+    data = []
+    for rec in result:
+        data.append(rec["name"] + "$" + rec["full_address"] + "$" + rec["city"] + "$" + rec["state"])
+
+    fh = open(saveLocation1, "w")
+    fh.write("\n".join(data))
+    fh.close()
+
 
 def FindBusinessBasedOnLocation(categoriesToSearch, myLocation, maxDistance, saveLocation2, collection):
-    pass
+    result = collection.find({"categories": {"$all": categoriesToSearch}})
+    data = []
+    for rec in result:
+        dist = get_distance_between_points(float(myLocation[0]), float(myLocation[1]), float(rec["latitude"]), float(rec["longitude"]))
+        if dist <= maxDistance:
+            data.append(rec["name"])
+
+    fh = open(saveLocation2, "w")
+    fh.write("\n".join(data))
+    fh.close()
 
